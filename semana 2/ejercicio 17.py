6fb359a0d88049a7aa43e46df2ff88043377819e
Jheferson Vargas ludeña <jhefefersonvargasludena@gmail.com>
2024-09-21 12:29:21 -0500

    Update ejercicio 17.py

--- semana 2/ejercicio 17.py
+++ semana 2/ejercicio 17.py
@@ -1,4 +1,5 @@
-
+#Verificar prefijo y sufijo: Escribe un programa que verifique si una cadena dada por el usuario
+#comienza con un prefijo específico y termina con un sufijo específico
 cadena = input("Introduce una cadena: ")
 prefijo = input("Introduce el prefijo a verificar: ")
 sufijo = input("Introduce el sufijo a verificar: ")
