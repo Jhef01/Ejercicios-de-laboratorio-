7a88783cd45af76e4d85732277c7fba83b1d3ed9
Jheferson Vargas ludeña <jhefefersonvargasludena@gmail.com>
2024-10-06 19:20:45 -0500

    agregar comentarios ejercicio 1.py

--- semana 6/ejercicio 1.py
+++ semana 6/ejercicio 1.py
@@ -1,17 +1,19 @@
-import random
-import string
+import random #este modulo permite seleccionar de modo aleatorio los caracteres para formar la contraseña.
+import string #usamos este modulo porque tiene varios caracteres que necitaremos para hacer el codigo .
 def generar_contrasena(longitud):
     while True:
-        caracteres = string.ascii_letters+ string.digits+string.punctuation 
-        contrasena=" ".join(random.choice(caracteres) for i in range(longitud))
-        if (any(c.isupper() for c in contrasena) and 
-            any(c.islower() for c in contrasena) and 
-            any(c.isdigit() for c in contrasena) and 
-            any(c in string.punctuation for c in contrasena)): 
+        caracteres = string.ascii_letters+ string.digits+string.punctuation  # Conjunto de caracteres disponibles (letras, dígitos y símbolos)
+        contrasena=" ".join(random.choice(caracteres) for i in range(longitud))  # Generar contraseña aleatoria sin espacios
+        # Verificar si la contraseña cumple con todos los requisitos
+        if (any(c.isupper() for c in contrasena) and # Al menos una mayúscula
+            any(c.islower() for c in contrasena) and # Al menos una minúscul
+            any(c.isdigit() for c in contrasena) and # Al menos un número
+            any(c in string.punctuation for c in contrasena)): # Al menos un símbolo
             return contrasena
+#programa principal 
 if __name__=="__main__":
     longitud_deseada=int(input("Ingrese la longitud deseada de la contraseña: "))
-    if longitud_deseada < 8:
+    if longitud_deseada < 8: #validar si la contraseña es mayor que igual que 8
         print("La contraseña debe tener al menos & caracteres.")
     else:
         contrasena_generada= generar_contrasena(longitud_deseada)
